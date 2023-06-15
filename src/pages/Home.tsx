dc4a43ec6cdc5277b5abf92587512909c5d75b84
Ethan Waldo <ethanwaldo9@gmail.com>
2023-06-15 16:21:41 -0400

    edit home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,20 @@
+import { Button, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import NavBar from "../components/NavBar";
 
 const Home = () => {
   return (
     <div>
       <NavBar />
+      <Container className="d-flex justify-content-center mt-4">
+        <Link to="/CameraKit">
+          <Button variant="dark" size="lg">
+            View our AR lenses
+          </Button>
+        </Link>
+      </Container>
     </div>
   );
-}
+};
 
 export default Home;
